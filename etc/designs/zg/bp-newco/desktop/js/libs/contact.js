5c1c9c2dd2faac91c03fba3e36de569b71912a5f
Aman Singh <amasingh44@publicisgroupe.net>
2022-06-09 17:12:13 +0530

    css add

--- etc/designs/zg/bp-newco/desktop/js/libs/contact.js
+++ etc/designs/zg/bp-newco/desktop/js/libs/contact.js
@@ -23,43 +23,52 @@
     inputVal.addEventListener('keyup', count);    
 
     function count(e) {
-        var len = inputVal.value.length;
-        countdisp.innerHTML = len;
+        var len = inputVal.value.trim();
+        countdisp.innerHTML = len.length;
     }
 
 
     $(document).ready(function () {
         $('#privacy').removeClass("external");
-        $('#name').on('keyup focus', function (event) {
+        $('#name').on('keyup focus keypress', function (event) {
             validateForm('name');
         });
-        $('#email').on('keyup focus', function (event) {
+        $('#email').on('keyup focus ', function (event) {
             validateForm('email');
         });
-        $('#description').on('keyup focus', function (event) {
+        $('#description').on('keyup focus keypress', function (event) {
             validateForm('desc');
         });
-        $('#00N9E000004hkmw').on('change keydown focus', function (event) {
+        $('#00N1q000000v7U5').on('change keydown focus', function (event) {
             validateForm('country');
         });
         $('#subject').on('change keydown focus', function (event) {
             validateForm('subject');
             if(event.type == 'change'){
                 let val = document.haleonForm.subject.value;
-                document.getElementById('enquiryReqMsg').style.cssText = "color: #9e9e9e; display:block";
-             if(val == 'Report a problem'){
-                document.getElementById("enquiryReqMsg").innerHTML = 'Please provide as much detail as possible, including: product variant, expiration date and LOT/BATCH numbers. In case of product quality reports, we might follow up and request a photo showing the defect.';
-                // $('#description').attr('placeholder', 'Please provide as much detail as possible, including: product variant, expiration date and LOT/BATCH numbers. In case of product quality reports, we might follow up and request a photo showing the defect.');
-             } 
-            else if(val == 'Brand or Product question'){
-                document.getElementById("enquiryReqMsg").innerHTML =  'Got a question to ask or something to tell us? We’re here for you.';
-             } 
-             else if(val == 'Anything else'){
-                document.getElementById("enquiryReqMsg").innerHTML =   'If your enquiry is related to commercial and distribution, promotions, sponsorship or anything else, we will point you in the right direction so your question is answered as quickly as possible.';
-             } 
-             else {
-                validateForm('subject');
-             } 
+                document.getElementById('subjectDescription').style.cssText = "color: #9e9e9e; display:block";
+            //  if(val == 'Report a problem'){
+            //     document.getElementById("subjectDescription").innerHTML = 'Please provide as much detail as possible, including: product variant, expiration date and LOT/BATCH numbers. In case of product quality reports, we might follow up and request a photo showing the defect.';
+            //     // $('#description').attr('placeholder', 'Please provide as much detail as possible, including: product variant, expiration date and LOT/BATCH numbers. In case of product quality reports, we might follow up and request a photo showing the defect.');
+            //  } 
+            // else if(val == 'Brand or Product question'){
+            //     document.getElementById("subjectDescription").innerHTML =  'Got a question to ask or something to tell us? We’re here for you.';
+            //  } 
+            //  else if(val == 'Anything else'){
+            //     document.getElementById("subjectDescription").innerHTML =   'If your enquiry is related to commercial and distribution, promotions, sponsorship or anything else, we will point you in the right direction so your question is answered as quickly as possible.';
+            //  } 
+            var subjectMap = {
+                "none": "",
+                "Brand or Product question": "Got a question to ask or something to tell us? We’re here for you.",
+                "Product Availability": "Please provide the details of the product variant in question, as well as your location. Our team will be able to advise of any out of stock items within our supply chain and estimate when the product is expected to be back in stock. ",
+                "Report a problem": "Please provide as much detail as possible, including: product variant, expiry date and LOT/BATCH numbers. In case of product quality reports we might follow up and request a photo showing the defect.",
+                "Anything else": "If your enquiry is related to commercial and distribution, promotions, sponsorship or anything else, we'll point you in the right direction so your question is answered as quickly as possible."
+            };
+           document.getElementById("subjectDescription").innerHTML = subjectMap[val];
+            validateForm('subject');
+            //  else {
+            //     validateForm('subject');
+            //  } 
             }
         });
        
@@ -116,15 +125,15 @@
             document.haleonForm.name.focus();
         } else if (document.haleonForm.email.value == '') {
             document.haleonForm.email.focus();
-        } else if (document.forms["haleonForm"]["00N9E000004hkmw"].value == '') {
-            document.forms["haleonForm"]["00N9E000004hkmw"].focus();
+        } else if (document.forms["haleonForm"]["00N1q000000v7U5"].value == '') {
+            document.forms["haleonForm"]["00N1q000000v7U5"].focus();
         } else if (document.haleonForm.subject.value == '') {
             document.haleonForm.subject.focus();
         } else if (document.haleonForm.description.value == '') {
             document.haleonForm.description.focus();
         }
-        else if (!document.haleonForm.checkbox_1.checked) {
-            document.haleonForm.checkbox_1.focus();
+        else if (!document.haleonForm.termsAndconditions.checked) {
+            document.haleonForm.termsAndconditions.focus();
         }
         else{
             ValidateEmail(event);
@@ -132,23 +141,24 @@
     }
 
     function validateForm(type) {
-        let name = document.forms["haleonForm"]["name"].value;
+        let name = document.forms["haleonForm"]["name"].value.trim();
         let email = document.forms["haleonForm"]["email"].value;
-        let country = document.forms["haleonForm"]["00N9E000004hkmw"].value;
+        let country = document.forms["haleonForm"]["00N1q000000v7U5"].value;
         let enquiry = document.forms["haleonForm"]["subject"].value;
-        let desc = document.forms["haleonForm"]["description"].value;
+        let desc = document.forms["haleonForm"]["description"].value.trim();
         let element = document.getElementById("emailGroup");
         if ((type == 'name' || type == 'all')) {
+            name.trim();
             if (name == "") {
-                document.getElementById('name').style.cssText = "border-color: #c30000; outline: 1px solid #c30000";
+                document.getElementById('name').style.cssText = "border-color: #c30000;";
                 document.getElementById("nameReqMsg").innerHTML = "Required";
                 document.getElementById('nameReqMsg').style.display = 'block';
             } else if (name.length < 5) {
-                document.getElementById('name').style.cssText = "border-color: #c30000; outline: 1px solid #c30000";
+                document.getElementById('name').style.cssText = "border-color: #c30000;";
                 document.getElementById("nameReqMsg").innerHTML = "This field must contain at least five characters.";
                 document.getElementById('nameReqMsg').style.display = 'block';
             } else {
-                document.getElementById('name').style.cssText = "border-color: #EAEAEA; outline: 1px solid #EAEAEA";
+                document.getElementById('name').style.cssText = "border-color: #EAEAEA;";
                 document.getElementById('nameReqMsg').style.display = 'none';
             }
         }
@@ -156,11 +166,11 @@
         if ((type == 'email' || type == 'all')) {
             if (email == '') {
                 element.classList.remove("emailValidation");
-                document.getElementById('email').style.cssText = "border-color: #c30000; outline: 1px solid #c30000";
+                document.getElementById('email').style.cssText = "border-color: #c30000;";
                 document.getElementById('emailError').style.display = 'none';
                 document.getElementById('emailReqMsg').style.display = 'block';
             } else {
-                document.getElementById('email').style.cssText = "border-color: #EAEAEA; outline: 1px solid #EAEAEA";
+                document.getElementById('email').style.cssText = "border-color: #EAEAEA;";
                 document.getElementById('emailReqMsg').style.display = 'none';
                 emailValidation();
             }
@@ -168,36 +178,37 @@
 
         if ((type == 'country' || type == 'all')) {
             if (country == "") {
-                document.getElementById('00N9E000004hkmw').style.cssText = "border-color: #c30000; outline: 1px solid #c30000";
+                document.getElementById('00N1q000000v7U5').style.cssText = "border-color: #c30000;";
                 document.getElementById('countryReqMsg').style.display = 'block';
             } else {
-                document.getElementById('00N9E000004hkmw').style.cssText = "border-color: #EAEAEA; outline: 1px solid #EAEAEA";
+                document.getElementById('00N1q000000v7U5').style.cssText = "border-color: #EAEAEA;";
                 document.getElementById('countryReqMsg').style.display = 'none';
             }
         }
         if ((type == 'subject' || type == 'all')) {
             if (enquiry == "") {
-                document.getElementById('subject').style.cssText = "border-color: #c30000; outline: 1px solid #c30000";
-                document.getElementById('enquiryReqMsg').style.cssText = "color: #c30000; display: block";
-                document.getElementById("enquiryReqMsg").innerHTML ="Required";
+                document.getElementById('subject').style.cssText = "border-color: #c30000;";
+                document.getElementById('subjectDescription').style.cssText = "color: #c30000; display: block";
+                document.getElementById("subjectDescription").innerHTML ="Required";
 
             } else {
-                document.getElementById('subject').style.cssText = "border-color: #EAEAEA; outline: 1px solid #EAEAEA";
+                document.getElementById('subject').style.cssText = "border-color: #EAEAEA;";
             }
         }
         if ((type == 'desc' || type == 'all')) {
+            desc = desc.trim();
             if (desc == "") {
-                document.getElementById('description').style.cssText = "border-color: #c30000; outline: 1px solid #c30000";
+                document.getElementById('description').style.cssText = "border-color: #c30000;";
                 document.getElementById("descReqMsg").innerHTML = "Required.";
                 document.getElementById('descReqMsg').style.display = 'block';
             } 
             else if (desc.length < 20) {
-                document.getElementById('description').style.cssText = "border-color: #c30000; outline: 1px solid #c30000";
+                document.getElementById('description').style.cssText = "border-color: #c30000;";
                 document.getElementById("descReqMsg").innerHTML = "This field must contain at least 20 characters.";
                 document.getElementById('descReqMsg').style.display = 'block';
             }
             else {
-                document.getElementById('description').style.cssText = "border-color: #EAEAEA; outline: 1px solid #EAEAEA";
+                document.getElementById('description').style.cssText = "border-color: #EAEAEA;";
                 document.getElementById('descReqMsg').style.display = 'none';
             }
         }
@@ -205,12 +216,16 @@
 
     function ValidateEmail(event) {
         var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-        if(document.haleonForm.name.value.length < 5){
+        let val = document.haleonForm.name.value;
+            val = val.trim();
+        if(val.length < 5){
             event.preventDefault();
             document.haleonForm.name.focus();
             return false;
         }
-        if(document.haleonForm.description.value.length < 20){
+        let text =document.haleonForm.description.value;
+            text = text.trim();
+        if(text.length < 20){
             event.preventDefault();
             document.haleonForm.description.focus();
             return false;
@@ -218,7 +233,7 @@
         if (document.haleonForm.email.value.match(mailformat)) {
                     document.haleonForm.retURL.value = `${window.location.origin}/contact-us/thank-you/`;
                     window.location.href = document.haleonForm.retURL.value;
-                    document.getElementById("enquiryReqMsg").innerHTML = '';
+                    document.getElementById("subjectDescription").innerHTML = '';
                    document.getElementById("haleonForm").submit();
                    setTimeout(document.haleonForm.reset(), 2000);
                    return true;
